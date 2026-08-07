a3b0fbf51c0d8b873690f6f755f1ae72708a4cf5
Wemerson Mota de Oliveira <wemersonatom@gmail.com>
2026-08-07 18:34:51 -0300

    chore: enable standalone for Docker

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 import type { NextConfig } from "next";
 
-const repo = "ventura-aifree";
-
 const nextConfig: NextConfig = {
-  output: "export",
-  trailingSlash: true,
-  images: { unoptimized: true },
-  basePath: process.env.NODE_ENV === "production" ? `/${repo}` : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? `/${repo}/` : "",
+  output: "standalone",
 };
 
 export default nextConfig;
